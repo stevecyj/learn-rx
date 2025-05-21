c8e1dbfa13db0325d29f204fe0d6cb64201665ca
steve.tsao <steve.tsao@hlwtech.com.tw>
2025-05-21 11:34:28 +0800

    feat: 在 HomeView.vue 中新增計數功能，使用 RxJS 進行每秒計數並在按鈕點擊時停止計數

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<script setup>
+import { from, fromEvent, useSubscription } from '@vueuse/rxjs'
+import { interval } from 'rxjs'
+import { map, mapTo, takeUntil, withLatestFrom } from 'rxjs/operators'
+import { shallowRef, useTemplateRef } from 'vue'
+
+const count = shallowRef(0)
+const button = useTemplateRef('buttonRef')
+
+useSubscription(
+  interval(1000)
+    .pipe(
+      mapTo(1),
+      takeUntil(fromEvent(button, 'click', { passive: true })),
+      withLatestFrom(
+        from(count, {
+          immediate: true,
+          deep: false,
+        }),
+      ),
+      map(([curr, total]) => curr + total),
+    )
+    .subscribe((val) => {
+      count.value = val
+      console.log('計數值：', val)
+    }), // 直接在 subscribe 回呼中更新 count 並印出 log
+)
+</script>
 
 <template>
-  <main></main>
+  <main>
+    {{ count }}
+    <button ref="buttonRef">Click me</button>
+  </main>
 </template>
