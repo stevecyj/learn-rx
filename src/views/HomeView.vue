44b30c399d64b07772743324d56efa4bcba57680
steve.tsao <steve.tsao@hlwtech.com.tw>
2025-05-21 17:14:39 +0800

    refactor: 在 HomeView.vue 中更新點擊事件的處理邏輯，新增節流功能以限制點擊事件的觸發頻率，並累加滑鼠點擊的 x 座標

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { from, fromEvent, useSubscription } from '@vueuse/rxjs'
-import { interval, scan } from 'rxjs'
+import { interval, scan, throttleTime } from 'rxjs'
 import { map, takeUntil, withLatestFrom } from 'rxjs/operators'
 import { shallowRef, useTemplateRef } from 'vue'
 
@@ -34,12 +34,35 @@ const button = useTemplateRef('buttonRef')
 //   }),
 // )
 
+// useSubscription(
+//   fromEvent(document, 'click')
+//     .pipe(scan((count) => count + 1, 0))
+//     .subscribe((count) => {
+//       console.log('計數值：', count)
+//     }),
+// )
+
+// 節流, 1秒內只會執行一次
+// useSubscription(
+//   fromEvent(document, 'click')
+//     .pipe(
+//       throttleTime(1000),
+//       scan((count) => count + 1, 0),
+//     )
+//     .subscribe((count) => {
+//       console.log('計數值：', count)
+//     }),
+// )
+
+// 累加滑鼠點擊的 x 座標
 useSubscription(
   fromEvent(document, 'click')
-    .pipe(scan((count) => count + 1, 0))
-    .subscribe((count) => {
-      console.log('計數值：', count)
-    }),
+    .pipe(
+      throttleTime(1000),
+      map((event) => event.clientX),
+      scan((count, clientX) => count + clientX, 0),
+    )
+    .subscribe((count) => console.log(count)),
 )
 </script>
 
