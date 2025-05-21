f2e5ec6751900c48c4f94801e37042170d1fe839
steve.tsao <steve.tsao@hlwtech.com.tw>
2025-05-21 16:48:17 +0800

    refactor: 更新 HomeView.vue 中的計數邏輯，將計數功能改為簡單的點擊事件訂閱，並註解掉原有計數邏輯

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,29 +8,36 @@ const count = shallowRef(0)
 const button = useTemplateRef('buttonRef')
 
 // 計數器
+// useSubscription(
+//   interval(1000)
+//     .pipe(
+//       map(() => 1),
+//       takeUntil(fromEvent(button, 'click', { passive: true })),
+//       withLatestFrom(
+//         from(count, {
+//           immediate: true,
+//           deep: false,
+//         }),
+//       ),
+//       map(([curr, total]) => curr + total),
+//     )
+//     .subscribe((val) => {
+//       count.value = val
+//       console.log('計數值：', val)
+//     }), // 直接在 subscribe 回呼中更新 count 並印出 log
+// )
+
+// 建立訂閱
 useSubscription(
-  interval(1000)
-    .pipe(
-      map(() => 1),
-      takeUntil(fromEvent(button, 'click', { passive: true })),
-      withLatestFrom(
-        from(count, {
-          immediate: true,
-          deep: false,
-        }),
-      ),
-      map(([curr, total]) => curr + total),
-    )
-    .subscribe((val) => {
-      count.value = val
-      console.log('計數值：', val)
-    }), // 直接在 subscribe 回呼中更新 count 並印出 log
+  fromEvent(document, 'click', { passive: true }).subscribe(() => {
+    console.log('Clicked!')
+  }),
 )
 </script>
 
 <template>
   <main>
-    {{ count }}
-    <button ref="buttonRef">Click me</button>
+    <!-- {{ count }}
+    <button ref="buttonRef">Click me</button> -->
   </main>
 </template>
